fbcdc9dd36c473e57865f62f61bef164587dea28
Rahul pratap singh <rahulprataps386@gmail.com>
2025-08-21 23:34:46 +0530

    add-cron-jobs

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,12 @@
+import User from "../models/User";
+
+export const protect = async (req, res, next) => {
+	const { userId } = req.auth;
+	if (!userId) {
+		return res.json({ success: false, message: "Not autherized" });
+	} else {
+		const user = await User.findById(userId);
+		req.user = user;
+		next();
+	}
+};
